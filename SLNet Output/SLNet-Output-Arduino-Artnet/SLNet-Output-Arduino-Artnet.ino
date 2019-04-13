c411a528f78678e6b71e92228fa0106de0df4b22
Benedikt <mail@benedikt-schwering.de>
2019-04-13 14:26:59 +0200

    Update SLNet-Output-Arduino-Artnet.ino

--- SLNet Output/SLNet-Output-Arduino-Artnet/SLNet-Output-Arduino-Artnet.ino
+++ SLNet Output/SLNet-Output-Arduino-Artnet/SLNet-Output-Arduino-Artnet.ino
@@ -6,16 +6,15 @@
 
 Artnet artnet;
 
+uint8_t dmx[512];
+
 // Change ip and mac address for your setup
 byte ip[] = {192, 168, 0, 151};
-byte broadcast[] = {10, 0, 1, 255};
 byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC};
 
 void setup()
 {
-  Serial.begin(115200);
   artnet.begin(mac, ip);
-  artnet.setBroadcast(broadcast);
 
   DmxSimple.usePin(3);
   DmxSimple.maxChannel(512);
@@ -26,23 +25,28 @@ void loop()
   uint16_t r = artnet.read();
   if(r == ART_POLL)
   {
-    Serial.println("POLL");
+    
   }
   if (r == ART_DMX)
   {
     if(artnet.getDmxFrame()[99]==0&&artnet.getDmxFrame()[100]==100)
     {
       for (int i = 0 ; i < artnet.getLength() ; i++)
-      {
+      {       
         int channel = i+1;
         int value = artnet.getDmxFrame()[i];
 
-        DmxSimple.write(channel, value);
-        
-        if(channel==1||channel==430)
-          analogWrite(9, value);
-        if(channel==2||channel==304)
-          analogWrite(6, value);
+        if(dmx[i] != artnet.getDmxFrame()[i])
+        {
+          dmx[i] = artnet.getDmxFrame()[i];
+          
+          if(channel==1||channel==430)
+            analogWrite(9, value);
+          if(channel==2||channel==304)
+            analogWrite(6, value);
+  
+          DmxSimple.write(channel, value);
+        }
       }
     }
   }
